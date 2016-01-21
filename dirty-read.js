73495e5a7d9899ac5ebdf4ab80b7f5487896adda
Mike Wooldridge <mike@hellion.org>
2016-01-20 22:53:29 -0800

    Clean up code

--- dirty-read.js
+++ dirty-read.js
@@ -1,5 +1,6 @@
 var request = require('request');
 
+// Set common options
 var mlRequest = request.defaults({
   'auth': {
       'user': 'admin',
@@ -8,61 +9,59 @@ var mlRequest = request.defaults({
     }
 });
 
-console.log('******** DIRTY READ TEST ********');
+var urlBase = 'http://localhost:8000/v1/';
 
+console.log('******** DIRTY READ TEST ********');
 mlRequest.post({
-    'uri': 'http://localhost:8000/v1/transactions?name=tx1'
+  'uri': urlBase + 'transactions?name=tx1'
+},
+function (error, resp, body) {
+  var tx1 = resp.headers.location.substring(17);
+  console.log('1.  T1 created: ' + tx1);
+  mlRequest.post({
+    'uri': urlBase + 'transactions?name=tx2'
   },
-  function (error, response, body) {
-    var tx1 = response.headers.location.substring(17);
-    console.log('1.  T1 created: ' + tx1);
-    // 2. Create second transaction (tx2)
-    mlRequest.post({
-      'uri': 'http://localhost:8000/v1/transactions?name=tx2'
+  function (error, resp, body) {
+    var tx2 = resp.headers.location.substring(17);
+    console.log('2.  T2 created: ' + tx2);
+    console.log('3.  T1 starting to write /foo.txt');
+    mlRequest.put({
+      'uri': urlBase + 'documents?uri=/foo.txt&txid=' + tx1,
+      'body': 'bar'
     },
-    function (error, response, body) {
-      var tx2 = response.headers.location.substring(17);
-      console.log('2.  T2 created: ' + tx2);
-      // 3. tx1 write /foo.txt
-      console.log('3.  T1 starting to write /foo.txt');
-      mlRequest.put({
-        'uri': 'http://localhost:8000/v1/documents?uri=/foo.txt&txid=' + tx1,
-        'body': 'bar'
+    function (error, resp, body) {
+      var result = resp.statusCode === 201 ? 'success' : 'failure';
+      console.log('4.  T1 finished writing /foo.txt: ' + result + ' (' + resp.statusCode + ')');
+      console.log('5.  T2 starting to read /foo.txt');
+      mlRequest.get({
+        'uri': urlBase + 'documents?uri=/foo.txt&txid=' + tx2
       },
-      function (error, response, body) {
-        var result = response.statusCode === 201 ? 'success' : 'failure';
-        console.log('4.  T1 finished writing /foo.txt: ' + result + ' (' + response.statusCode + ')');
-        console.log('5.  T2 starting to read /foo.txt');
-        mlRequest.get({
-          'uri': 'http://localhost:8000/v1/documents?uri=/foo.txt&txid=' + tx2
+      function (error, resp, body) {
+        var result = resp.statusCode === 200 ? 'success' : 'failure';
+        console.log('6.  T2 finished reading /foo.txt: ' + result + ' (' + resp.statusCode + ')');
+        console.log('7.  T2 starting commit');
+        mlRequest.post({
+          'uri': urlBase + 'transactions/' + tx2 + '?result=commit'
         },
-        function (error, response, body) {
-          var result = response.statusCode === 200 ? 'success' : 'failure';
-          console.log('6.  T2 finished reading /foo.txt: ' + result + ' (' + response.statusCode + ')');
-          console.log('7.  T2 starting commit');
-          mlRequest.post({
-            'uri': 'http://localhost:8000/v1/transactions/' + tx2 + '?result=commit'
-          },
-          function (error, response, body) {
-            var result = response.statusCode === 204 ? 'success' : 'failure';
-            console.log('8.  T2 finished commit: ' + result + ' (' + response.statusCode + ')');
-          });
+        function (error, resp, body) {
+          var result = resp.statusCode === 204 ? 'success' : 'failure';
+          console.log('8.  T2 finished commit: ' + result + ' (' + resp.statusCode + ')');
         });
-        // Pause to ensure read request has been sent
-        setTimeout(function(){
-            var action = 'rollback';
-            console.log('9.  T1 starting rollback');
-            mlRequest.post({
-              'uri': 'http://localhost:8000/v1/transactions/' + tx1 + '?result=' + action
-            },
-            function (error, response, body) {
-              var result = response.statusCode === 204 ? 'success' : 'failure';
-              console.log('10. T1 finished rollback: ' + result + ' (' + response.statusCode + ')');
-            });
-          },
-          2000
-        );
       });
+      // Pause to ensure read request has been sent
+      setTimeout(function(){
+          var action = 'rollback';
+          console.log('9.  T1 starting rollback');
+          mlRequest.post({
+            'uri': urlBase + 'transactions/' + tx1 + '?result=' + action
+          },
+          function (error, resp, body) {
+            var result = resp.statusCode === 204 ? 'success' : 'failure';
+            console.log('10. T1 finished rollback: ' + result + ' (' + resp.statusCode + ')');
+          });
+        },
+        2000
+      );
     });
-  }
-);
+  });
+});
--- non-repeatable-read.js
+++ non-repeatable-read.js
@@ -1,5 +1,6 @@
 var request = require('request');
 
+// Set common options
 var mlRequest = request.defaults({
   'auth': {
       'user': 'admin',
@@ -8,98 +9,85 @@ var mlRequest = request.defaults({
     }
 });
 
-console.log('******** NON-REPEATABLE READ TEST ********');
+var urlBase = 'http://localhost:8000/v1/';
 
-mlRequest({
-    'uri': 'http://localhost:8000/v1/documents?uri=/foo.txt',
-    'method': 'PUT',
-    'body': '0'
+console.log('******** NON-REPEATABLE READ TEST ********');
+mlRequest.put({
+  'uri': urlBase + 'documents?uri=/foo.txt',
+  'body': '0'
+},
+function (error, response, body) {
+  var result = (response.statusCode === 201 || response.statusCode === 204) ? 'success' : 'failure';
+  console.log('1.  Wrote /foo.txt: ' + result + ' (' + response.statusCode + ')');
+  mlRequest.post({
+    'uri': urlBase + 'transactions?name=tx1'
   },
   function (error, response, body) {
-    var result = (response.statusCode === 201 || response.statusCode === 204) ? 'success' : 'failure';
-    console.log('1.  Wrote /foo.txt: ' + result + ' (' + response.statusCode + ')');
-    mlRequest({
-      'uri': 'http://localhost:8000/v1/transactions?name=tx1',
-      'method': 'POST'
+    var tx1 = response.headers.location.substring(17);
+    console.log('2.  T1 created: ' + tx1);
+    mlRequest.post({
+      'uri': urlBase + 'transactions?name=tx2'
     },
     function (error, response, body) {
-      var tx1 = response.headers.location.substring(17);
-      console.log('2.  T1 created: ' + tx1);
-      mlRequest({
-        'uri': 'http://localhost:8000/v1/transactions?name=tx2',
-        'method': 'POST'
+      var tx2 = response.headers.location.substring(17);
+      console.log('3.  T2 created: ' + tx2);
+      console.log('4.  T1 starting to read /foo.txt');
+      mlRequest.get({
+        'uri': urlBase + 'documents?uri=/foo.txt&txid=' + tx1
       },
       function (error, response, body) {
-        var tx2 = response.headers.location.substring(17);
-        console.log('3.  T2 created: ' + tx2);
-        console.log('4.  T1 starting to read /foo.txt');
-        mlRequest({
-          'uri': 'http://localhost:8000/v1/documents?uri=/foo.txt&txid=' + tx1,
-          'method': 'GET'
+        console.log('5.  T1 finished reading /foo.txt, value: ' + body);
+        // 5. tx2 write /foo.txt
+        console.log('6.  T2 starting to write /foo.txt, value: 2');
+        mlRequest.put({
+          'uri': urlBase + 'documents?uri=/foo.txt&txid=' + tx2,
+          'body': '2'
         },
         function (error, response, body) {
-          console.log('5.  T1 finished reading /foo.txt, value: ' + body);
-          // 5. tx2 write /foo.txt
-          console.log('6.  T2 starting to write /foo.txt, value: 2');
-          mlRequest({
-            'uri': 'http://localhost:8000/v1/documents?uri=/foo.txt&txid=' + tx2,
-            'method': 'PUT',
-            'body': '2'
+          var result = response.statusCode === 204 ? 'success' : 'failure';
+          console.log('7.  T2 finished writing /foo.txt: ' + result + ' (' + response.statusCode + ')');
+          console.log('8.  T2 starting to commit');
+          mlRequest.post({
+            'uri': urlBase + 'transactions/' + tx2 + '?result=commit'
           },
           function (error, response, body) {
             var result = response.statusCode === 204 ? 'success' : 'failure';
-            console.log('7.  T2 finished writing /foo.txt: ' + result + ' (' + response.statusCode + ')');
-            console.log('8.  T2 starting to commit');
-            mlRequest({
-              'uri': 'http://localhost:8000/v1/transactions/' + tx2 + '?result=commit',
-              'method': 'POST'
+            console.log('9.  T2 finished commit: ' + result + ' (' + response.statusCode + ')');
+            mlRequest.get({
+              'uri': urlBase + 'documents?uri=/foo.txt'
+            },
+            function (error, response, body) {
+              console.log('10. Read /foo.txt, value: ' + body);
+            });
+            mlRequest.del({
+              'uri': urlBase + 'documents?uri=/foo.txt'
+            },
+            function (error, response, body) {
+              console.log('11. Deleted /foo.txt');
+            });
+          });
+        });
+        // Pause to ensure write request has been sent
+        setTimeout(function(){
+          console.log('12. T1 starting to read /foo.txt');
+          mlRequest.get({
+            'uri': urlBase + 'documents?uri=/foo.txt&txid=' + tx1
+          },
+          function (error, response, body) {
+            console.log('13. T1 finished reading /foo.txt, value: ' + body);
+            console.log('14. T1 starting to commit');
+            mlRequest.post({
+              'uri': urlBase + 'transactions/' + tx1 + '?result=commit'
             },
             function (error, response, body) {
               var result = response.statusCode === 204 ? 'success' : 'failure';
-              console.log('9.  T2 finished commit: ' + result + ' (' + response.statusCode + ')');
-              mlRequest({
-                'uri': 'http://localhost:8000/v1/documents?uri=/foo.txt',
-                'method': 'GET'
-              },
-              function (error, response, body) {
-                console.log('10. Read /foo.txt, value: ' + body);
-              });
-              mlRequest({
-                'uri': 'http://localhost:8000/v1/documents?uri=/foo.txt',
-                'method': 'DELETE'
-              },
-              function (error, response, body) {
-                console.log('11. Deleted /foo.txt');
-              });
-
+              console.log('15. T1 finished commit: ' + result + ' (' + response.statusCode + ')');
             });
           });
-
-          // Pause to ensure write request has been sent
-          setTimeout(function(){
-            console.log('12. T1 starting to read /foo.txt');
-            mlRequest({
-              'uri': 'http://localhost:8000/v1/documents?uri=/foo.txt&txid=' + tx1,
-              'method': 'GET'
-            },
-            function (error, response, body) {
-              console.log('13. T1 finished reading /foo.txt, value: ' + body);
-              console.log('14. T1 starting to commit');
-              mlRequest({
-                'uri': 'http://localhost:8000/v1/transactions/' + tx1 + '?result=commit',
-                'method': 'POST'
-              },
-              function (error, response, body) {
-                var result = response.statusCode === 204 ? 'success' : 'failure';
-                console.log('15. T1 finished commit: ' + result + ' (' + response.statusCode + ')');
-              });
-            });
-          },
-          2000
-        );
-
-        });
+        },
+        2000
+      );
       });
     });
-  }
-);
+  });
+});
